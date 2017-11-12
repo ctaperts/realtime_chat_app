7739d446b3e76c38eacbd0fa187c08bb060dae85
Colby Taperts <colbytaperts@gmail.com>
2017-11-11 21:11:23 -0600

    add in input for list

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-let channels = [
-  {name: 'Hardware Support'},
-  {name: 'Software Support'}
-];
 
 
 class Channel extends React.Component{
@@ -44,14 +40,11 @@ class ChannelForm extends React.Component{
   onSubmit(e){
     let {channelName} = this.state;
     console.log(channelName);
-    channels.push({
-      name: channelName
-    });
     this.setState({
       channelName: ''
     });
+    this.props.addChannel(channelName);
     e.preventDefault();
-
   }
   render(){
     return(
@@ -66,11 +59,27 @@ class ChannelForm extends React.Component{
 }
 
 class ChannelSection extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      channels: [
+        {name: 'Hardware Support'},
+        {name: 'Software Support'}
+      ]
+    };
+  }
+  addChannel(name){
+    let {channels} = this.state;
+    channels.push({name: name});
+    this.setState({
+      channels: channels
+    });
+  }
   render(){
     return(
     <div>
-      <ChannelList channels={channels} />
-      <ChannelForm />
+      <ChannelList channels={this.state.channels} />
+      <ChannelForm addChannel={this.addChannel.bind(this)} />
     </div>
     )
   }
