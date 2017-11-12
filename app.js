f01c8395b25aa09a8c40b07b715db8af949fc652
Colby Taperts <colbytaperts@gmail.com>
2017-11-11 20:51:12 -0600

    setup form with clear

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let channels = [
+  {name: 'Hardware Support'},
+  {name: 'Software Support'}
+];
+
+
 class Channel extends React.Component{
   onClick(){
     console.log("test", this.props.name);
@@ -9,5 +15,66 @@ class Channel extends React.Component{
   }
 }
 
-ReactDOM.render(<Channel name='Hardware Support'/>,
+class ChannelList extends React.Component{
+  render(){
+    return (
+      <ul>
+      {this.props.channels.map( channel => {
+        return (
+          <Channel name={channel.name} />
+        )
+      }
+      )}
+      </ul>
+    )
+  }
+}
+
+class ChannelForm extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  onChange(e){
+    this.setState({
+      channelName: e.target.value
+    });
+    // console.log(e.target.value);
+  }
+  onSubmit(e){
+    let {channelName} = this.state;
+    console.log(channelName);
+    channels.push({
+      name: channelName
+    });
+    this.setState({
+      channelName: ''
+    });
+    e.preventDefault();
+
+  }
+  render(){
+    return(
+    <form onSubmit={this.onSubmit.bind(this)}>
+      <input type='text'
+        onChange={this.onChange.bind(this)}
+        value={this.state.channelName}
+      />
+    </form>
+    )
+  }
+}
+
+class ChannelSection extends React.Component{
+  render(){
+    return(
+    <div>
+      <ChannelList channels={channels} />
+      <ChannelForm />
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<ChannelSection />,
   document.getElementById('app'));
