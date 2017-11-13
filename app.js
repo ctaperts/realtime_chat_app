ea4821192f37a4986c13e0755a2ed19f0e96a828
Colby Taperts <colbytaperts@gmail.com>
2017-11-12 22:10:25 -0600

    init app

--- app.js
+++ app.js
@@ -1,89 +1,89 @@
 
 
 class Channel extends React.Component{
-  onClick(){
-    console.log("test", this.props.name);
-  }
-  render(){
-    return (
-      <li onClick={this.onClick.bind(this)}>{this.props.name}</li>
-    )
-  }
+onClick(){
+  console.log("test", this.props.name);
+}
+render(){
+  return (
+    <li onClick={this.onClick.bind(this)}>{this.props.name}</li>
+  )
+}
 }
 
 class ChannelList extends React.Component{
-  render(){
-    return (
-      <ul>
+render(){
+  return (
+    <ul>
       {this.props.channels.map( channel => {
         return (
           <Channel name={channel.name} />
         )
       }
       )}
-      </ul>
-    )
-  }
+    </ul>
+  )
+}
 }
 
 class ChannelForm extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {};
-  }
-  onChange(e){
-    this.setState({
-      channelName: e.target.value
-    });
-    // console.log(e.target.value);
-  }
-  onSubmit(e){
-    let {channelName} = this.state;
-    console.log(channelName);
-    this.setState({
-      channelName: ''
-    });
-    this.props.addChannel(channelName);
-    e.preventDefault();
-  }
-  render(){
-    return(
+constructor(props){
+  super(props);
+  this.state = {};
+}
+onChange(e){
+  this.setState({
+    channelName: e.target.value
+  });
+  // console.log(e.target.value);
+}
+onSubmit(e){
+  let {channelName} = this.state;
+  console.log(channelName);
+  this.setState({
+    channelName: ''
+  });
+  this.props.addChannel(channelName);
+  e.preventDefault();
+}
+render(){
+  return(
     <form onSubmit={this.onSubmit.bind(this)}>
-      <input type='text'
-        onChange={this.onChange.bind(this)}
-        value={this.state.channelName}
-      />
+    <input type='text'
+      onChange={this.onChange.bind(this)}
+      value={this.state.channelName}
+    />
     </form>
-    )
-  }
+  )
+}
 }
 
 class ChannelSection extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      channels: [
-        {name: 'Hardware Support'},
-        {name: 'Software Support'}
-      ]
-    };
-  }
-  addChannel(name){
-    let {channels} = this.state;
-    channels.push({name: name});
-    this.setState({
-      channels: channels
-    });
-  }
-  render(){
-    return(
+constructor(props){
+  super(props);
+  this.state = {
+    channels: [
+      {name: 'Hardware Support'},
+      {name: 'Software Support'}
+    ]
+  };
+}
+addChannel(name){
+  let {channels} = this.state;
+  channels.push({name: name});
+  this.setState({
+    channels: channels
+  });
+}
+render(){
+  return(
     <div>
       <ChannelList channels={this.state.channels} />
       <ChannelForm addChannel={this.addChannel.bind(this)} />
     </div>
-    )
-  }
+  )
+}
 }
 
 ReactDOM.render(<ChannelSection />,
-  document.getElementById('app'));
+document.getElementById('app'));
