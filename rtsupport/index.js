33bd3f7601a6369f06b0fd8e260ba0928d111012
Colby Taperts <colbytaperts@gmail.com>
2017-11-17 21:27:06 -0600

    set up inital app

--- rtsupport/index.js
+++ rtsupport/index.js
@@ -1,2 +1,8 @@
-alert("this is a alert");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
 
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
